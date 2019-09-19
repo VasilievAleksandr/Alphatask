ed8b4b7a20a5483126e91de9a76337c057cb77cb
Shurferer <40690225+Shurferer@users.noreply.github.com>
2019-09-19 21:55:34 +0300

    Create bsearch function

--- src/AlphaTask.java
+++ src/AlphaTask.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+public class AlphaTask {
 
-public class Alphatask {
+    public static void main(String args[]) {
+
+        AlphaTask testBean = new AlphaTask();
+        System.out.println(testBean.bsearch(new int[] {100, 50, 40, 30, 20, 10, 1}, 15));
+    }
+
+
+    public int bsearch (int[] array, int x){
+        int result=-1;
+        for (int i=0; i<array.length; i++){
+            if (array[i]<x){
+                result=i;
+                break;
+            }
+        }
+        return result;
+    }
 }
